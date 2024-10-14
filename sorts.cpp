9e61d9680f4f4672ee0651de392457b57b78785b
Ana Carolina Coronel <A00838713@tec.mx>
2024-10-13 22:37:28 -0600

    Update sorts.cpp

--- sorts.cpp
+++ sorts.cpp
@@ -1,47 +1,42 @@
 #include "sorts.h"
-//Divide recursivamente el vector y luego los junta en orden. 
 
+// Divide recursivamente el vector y luego los junta en orden. 
 void Sorts::ordenaMerge(std::vector<Song> &v) {
-    std::vector<Song> tmp(v.size()); // Vector temporal del tamano v 
-    mergeSplit(v, tmp, 0, v.size() - 1); 
+    std::vector<Song> tmp(v.size());
+    mergeSplit(v, tmp, 0, v.size() - 1);
 }
 
-void Sorts::mergeSplit(std::vector<Song> &A, std::vector<Song> &B, int low, int high) {
-    if (low >= high){
+void Sorts::mergeSplit(std::vector<Song> &A, std::vector<Song> &B, int left, int right) {
+    if (left < right) {
+        return; }
+        int mid = (left + right) / 2;
+        mergeSplit(A, B, left, mid);
+        mergeSplit(A, B, mid + 1, right);
+        mergeArray(A, B, left, mid, right);
+    }
 
-    return; } // Caso inicial si ya esta ordenado
-
-    int mid = (low + high) / 2; // Calcula la mitad 
-    mergeSplit(A, B, low, mid); // Ordena lado izquierdo
-    mergeSplit(A, B, mid + 1, high); // Ordena lado derecho
-    mergeArray(A, B, low, mid, high); 
-}
 
 void Sorts::mergeArray(std::vector<Song> &A, std::vector<Song> &B, int low, int mid, int high) {
     int i = low, j = mid + 1, k = low;
 
-    
     while (i <= mid && j <= high) {
-        if (A[i].getAno() < A[j].getAno() ||
-            (A[i].getAno() == A[j].getAno() && A[i].getTit() < A[j].getTit())) {
+        if (A[i].getAno() < A[j].getAno() || 
+            (A[i].getAno() == A[j].getAno() && A[i].getTitle() < A[j].getTitle())) {
             B[k++] = A[i++];
         } else {
             B[k++] = A[j++];
         }
     }
 
-   
     while (i <= mid) {
         B[k++] = A[i++];
     }
 
-   
     while (j <= high) {
         B[k++] = A[j++];
     }
 
-
-    for (i = low; i <= high; i++) {
-        A[i] = B[i];
+    for (int l = low; l <= high; l++) {
+        A[l] = B[l];
     }
 }
