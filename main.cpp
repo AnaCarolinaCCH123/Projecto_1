ba9461c9332d40014dd0ac45a0604c91e33f78e9
Ana Carolina Coronel <A00838713@tec.mx>
2024-10-13 22:34:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,66 +1,87 @@
+#include <iostream>
+#include <string>
+#include <vector>
 #include "song.h"
 #include "sorts.h"
-#include <iostream>
-#include <vector>
+#include "dlink.h"
 
 int main() {
-    std::vector<Song> canciones; //Vector que almacena las canciones 
-    Sorts sorts;
-    int num;
+
+   
+    DList<Song> songList; // Template DList 
+    Sorts sorter; // Sorts para ordenar 
+    int option = 0;
 
     do {
-    
-        std::cout << "Opciones del programa:\n";
-        std::cout << "1. Ingresar canciones\n";
-        std::cout << "2. Ver lista de canciones ordenadas\n";
-        std::cout << "3. Salir\n";
-        std::cout << "Elige la opcion: ";
-        std::cin >> num;
+        std::cout << "El presente porgrama pretende simular una playlist de musica\n";
+        std::cout << "Este es el menu de opciones\n";
+        std::cout << "1. Agregar una nueva cancion al inicio\n";
+        std::cout << "2. Eliminar cancion por ano\n";
+        std::cout << "3. Eliminar cancion por titulo\n";
+        std::cout << "4. Ver lista de canciones ordenadas por ano\n";
+        std::cout << "5. Salir\n";
+        std::cout << "Ingrese una opcion: ";
+        std::cin >> option;
 
-        switch (num) {
-            case 1: {
-                int numCanciones;
-                std::cout << "Numero de canciones a ingresar: ";
-                std::cin >> numCanciones;
+        switch (option) {
 
-                for (int i = 0; i < numCanciones; ++i) {
-                    std::string titulo;
-                    int ano;
+        case 1: {
 
-                    std::cout << "Ingresa el titulo de la cancion:\n";
-                    std::cin.ignore(); 
-                    std::getline(std::cin, titulo);
+            std::string title;
+            int year;
 
-                    std::cout << "Ingresa su ano de publicacion de:\n";
-                    std::cin >> ano;
+            std::cout << "TItulo de la canciOn: ";
+            std::cin.ignore(); 
+            std::getline(std::cin, title);
+            std::cout << "Ingrese su ano de publicacion: ";
+            std::cin >> year;
 
-                    canciones.emplace_back(titulo, ano);
-                    std::cout << "Canciones agregadas\n";
-                }
-                break;  
-            }
-            case 2: {
-                if (canciones.empty()) {
-                    std::cout << "No hay canciones \n";
-                } else {
-                    // Ordenar las canciones
-                    sorts.ordenaMerge(canciones);
-
-                    // Mostrar canciones ordenadas
-                    std::cout << "Lista de canciones ordenada segun su ano de publicacion:\n";
-                    for (const auto &cancion : canciones) {
-                        std::cout << cancion.getTit() << ", " << cancion.getAno() << std::endl;
-                    }
-                }
-                break;
-            }
-            case 3: {
-                std::cout << "Saliendo del programa...\n";
-                break;
-            }
-           
+            // Objeto Song 
+            Song nuevaCancion(year, title);
+            songList.addFirst(nuevaCancion);
+            std::cout << "Cancion agregada correctamente.\n";
+            break;
         }
-    } while (num != 3); 
+
+        case 2: {
+            int year;
+            std::cout << "Ingrese el ano de la cancion a eliminar: ";
+            std::cin >> year;
+            songList.removeYear(year);
+            break;
+        }
+
+        case 3: {
+            std::string title;
+            std::cout << "Ingrese el titulo de la cancion a eliminar: ";
+            std::cin.ignore();
+            std::getline(std::cin, title);
+            songList.removeTitle(title);
+            break;
+        }
+
+        case 4: {
+             std::vector<Song> songVector = songList.toVector(); 
+             std::vector<Song> tempList(songVector.size()); //  vector temporal
+             
+             //Merge Sort
+             sorter.ordenaMerge(songVector);
+           
+            std::cout << "Lista de canciones ordenadas:\n";
+            for (const auto &cancion : songVector) {
+                std::cout << cancion.getTitle() << ", " << cancion.getAno() << std::endl;
+            }
+    
+    break;
+}        
+        case 5:
+            std::cout << "Saliendo del programa\n";
+            break;
+        default:
+            std::cout << "Erro 404\n";
+        }
+
+    } while (option != 5);
 
     return 0;
 }
